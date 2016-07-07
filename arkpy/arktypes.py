a3ec876bc8aabf290e056222f6df4306801050b3
Derek Ries <influenztial@gmail.com>
2016-07-07 10:19:44 -0700

    refactored code into method to load and set next prop on structs

--- arkpy/arktypes.py
+++ arkpy/arktypes.py
@@ -16,99 +16,6 @@ import json
 from binary import BinaryStream
 
 
-class BaseProperty:
-  def __init__(self, value=None):
-    # print 'Base Property init'
-    self.value=value
-    self.index = 0
-  def __repr__(self):
-    return str(self.value)
-  def __str__(self):
-    return str(self.value)
-
-
-class BaseStruct:
-  def __init__(self, size=None):
-    self.data = {}
-    self.size = 0
-  # def __repr__(self):
-  #   return json.dumps(self.data)
-  # def __str__(self):
-  #   return json.dumps(self.data)
-
-
-class StrProperty(BaseProperty):
-  def __init__(self, value='', stream=None):
-    BaseProperty.__init__(self)
-    self.value = value
-    if stream is not None:
-      garbage = stream.readInt32()
-      self.index = stream.readInt32()
-      self.value = stream.readNullTerminatedString()
-
-  def __repr__(self):
-    return self.value
-
-
-class ArrayProperty:
-  def __init__(self, value=[], type='IntProperty'):
-    self.value = value
-
-
-class FloatProperty:
-  def __init__(self, value=0.0):
-    self.value = value
-
-
-class DoubleProperty:
-  def __init__(self, value=0.0):
-    self.value = value
-
-
-class Int16Property:
-  def __init__(self, value=0):
-    self.value = value
-
-
-class UInt16Property:
-  def __init__(self, value=0):
-    self.value = value
-
-
-class IntProperty:
-  def __init__(self, value=0):
-    self.value = value
-
-
-class UIntProperty:
-  def __init__(self, value=0):
-    self.value = value
-
-
-class Int64Property:
-  def __init__(self, value=0):
-    self.value = value
-
-
-class UInt64Property(BaseProperty):
-  def __init__(self, value=0, stream=None):
-    BaseProperty.__init__(self, value=value)
-    self.value = value
-    if stream is not None:
-      stream.readBytes(8)
-      self.value = stream.readUInt64()
-
-
-class BoolProperty(BaseProperty):
-  def __init__(self, value=True, stream=None):
-    BaseProperty.__init__(self)
-    self.value = value
-    if stream is not None:
-      garbage = stream.readInt32()
-      self.index = stream.readInt32()
-      self.value = bool(stream.readChar())
-
-
 def load_struct(stream):
   size = stream.readInt32()
   index = stream.readInt32()
@@ -127,6 +34,137 @@ def load_property(stream):
   else:
     return (name, prop_type, PROPERTIES[prop_type](stream=stream))
 
+
+class BaseProperty:
+  def __init__(self, value=None):
+    # print 'Base Property init'
+    self.value=value
+    self.index = 0
+    self.size = 0
+  def __repr__(self):
+    return str(self.value)
+  def __str__(self):
+    return str(self.value)
+
+
+class BaseStruct:
+  def __init__(self, size=None):
+    self.data = {}
+    self.size = 0
+    self.index = 0
+
+  def load_and_set_next_property(self, stream):
+    """
+    Reads and automatically sets the next property value to
+    the appropriate field on the data dict of this struct. Since
+    some fields in Ark File formats use the same name, but are not
+    an array, their differentiator is a little Int32 index field.
+    """
+    name, prop_type, value = load_property(stream)
+    if value.index != 0:
+      first_item = self.data.get(name, None)
+      if first_item is None:
+        self.data[name] = []
+      elif isinstance(first_item, list) is False:
+        self.data[name] = [first_item]
+      self.data[name].append((prop_type, value))
+    else:
+      self.data[name] = (prop_type, value)
+    print self.data
+    # print value.index
+  # def __repr__(self):
+  #   return json.dumps(self.data)
+  # def __str__(self):
+  #   return json.dumps(self.data)
+
+
+class StrProperty(BaseProperty):
+  def __init__(self, value='', stream=None):
+    BaseProperty.__init__(self)
+    self.value = value
+    self.size = 4
+    if stream is not None:
+      self.size = stream.readInt32()
+      self.index = stream.readInt32()
+      self.value = stream.readNullTerminatedString()
+
+  def __repr__(self):
+    return self.value
+
+
+class ArrayProperty:
+  def __init__(self, value=[], type='IntProperty'):
+    self.value = value
+
+
+class FloatProperty(BaseProperty):
+  def __init__(self, value=0.0, stream=None):
+    BaseProperty.__init__(self)
+    self.value = value
+    self.size = 4
+    if stream is not None:
+      self.size = stream.readInt32()
+      self.index = stream.readInt32()
+      self.value = stream.readFloat()
+
+
+class DoubleProperty:
+  def __init__(self, value=0.0):
+    self.value = value
+    self.size = 8
+
+
+class Int16Property:
+  def __init__(self, value=0):
+    self.value = value
+    self.size = 2
+
+
+class UInt16Property:
+  def __init__(self, value=0):
+    self.value = value
+    self.size = 2
+
+
+class IntProperty:
+  def __init__(self, value=0):
+    self.value = value
+    self.size = 4
+
+
+class UIntProperty:
+  def __init__(self, value=0):
+    self.value = value
+    self.size = 4
+
+
+class Int64Property:
+  def __init__(self, value=0):
+    self.value = value
+    self.size = 8
+
+
+class UInt64Property(BaseProperty):
+  def __init__(self, value=0, stream=None):
+    BaseProperty.__init__(self, value=value)
+    self.value = value
+    self.size = 8
+    if stream is not None:
+      self.size = stream.readInt32()
+      self.index = stream.readInt32()
+      self.value = stream.readUInt64()
+
+
+class BoolProperty(BaseProperty):
+  def __init__(self, value=True, stream=None):
+    BaseProperty.__init__(self)
+    self.value = value
+    if stream is not None:
+      garbage = stream.readInt32()
+      self.index = stream.readInt32()
+      self.value = bool(stream.readChar())
+
+
 # ArkProfile Structures ----------------------------------
 
 class PrimalPlayerDataStruct(BaseStruct):
@@ -137,31 +175,17 @@ class PrimalPlayerDataStruct(BaseStruct):
     self.size = size
     if stream is not None:
       print 'loading from stream'
-      name, prop_type, value = load_property(stream)
-      self.data[name] = (prop_type, value)
-      print self.data
-      # load_property(stream)
-      print '---------------------------------'
+      self.load_and_set_next_property(stream)
+      # print '---------------------------------'
+      self.load_and_set_next_property(stream)
+      # print '---------------------------------'
+      self.load_and_set_next_property(stream)
+      # print '---------------------------------'
+      self.load_and_set_next_property(stream)
+      # print '---------------------------------'
 
-      name, prop_type, value = load_property(stream)
-      self.data[name] = (prop_type, value)
-      print self.data
-      print '---------------------------------'
-
-      name, prop_type, value = load_property(stream)
-      self.data[name] = (prop_type, value)
-      print self.data
-      print '---------------------------------'
-
-      name, prop_type, value = load_property(stream)
-      self.data[name] = (prop_type, value)
-      print self.data
-      print '---------------------------------'
-
-      name, prop_type, value = load_property(stream)
-      self.data[name] = (prop_type, value)
-      print self.data
-      print '---------------------------------'
+      self.load_and_set_next_property(stream)
+      # print '---------------------------------'
 
   def __write_to_binary_stream(self, stream):
     pass
@@ -188,15 +212,13 @@ class PrimalPlayerCharacterConfigStruct(BaseStruct):
     BaseStruct.__init__(self, size=size)
     self.size = size
     if stream is not None:
-      name, prop_type, value = load_property(stream)
-      self.data[name] = (prop_type, value)
-      print self.data
-      print value.index
+      # Colors
+      self.load_and_set_next_property(stream)
+      self.load_and_set_next_property(stream)
+      self.load_and_set_next_property(stream)
+      self.load_and_set_next_property(stream)
+      self.load_and_set_next_property(stream)
 
-      name, prop_type, value = load_property(stream)
-      self.data[name] = (prop_type, value)
-      print self.data
-      print value.index
 
   def __write_to_binary_stream(self, stream):
     pass
