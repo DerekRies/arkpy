414cbed1515c66eabef734acc044f6e67f045cab
Derek Ries <influenztial@gmail.com>
2016-07-27 12:33:39 -0700

    tests conforming to pep8

--- tests/test_ark.py
+++ tests/test_ark.py
@@ -11,88 +11,102 @@ output_dir = 'tests/output/'
 
 
 class TestArkProfile:
-  def test_write_read(self):
-    profile = ark.ArkProfile()
-    profile.map_name = ark.GameMapMap.the_center
-    profile.map_path = ark.GameMapMap.the_center_path
-    profile.unique_id.set('11111111111111111')
-    profile.player_name.set('Fake Steam Name')
-    profile.player_id.set(utils._gen_player_id())
-    profile.first_spawned.set(True)
-    profile.character.name.set('GeneratedCharacter')
-    profile.character.level_ups.set(9)
-    profile.character.experience.set(450)
-    profile.character.stat_points[ark.StatMap.Health].set(9)
-    profile.save_to_file(output_dir + 'generatedprofile.arkprofile')
 
-    # TODO: Create some functionality to compare the data dicts directly
-    # to see if they are the same
-    profile2 = ark.ArkProfile(output_dir + 'generatedprofile.arkprofile')
-    assert profile2.map_name == profile.map_name
-    assert profile2.map_path == profile.map_path
-    assert profile2.unique_id.value == profile.unique_id.value
-    assert profile2.player_name.value == profile.player_name.value
-    assert profile2.player_id.value == profile.player_id.value
-    assert profile2.character.isFemale.value == profile.character.isFemale.value
-    assert profile2.character.stat_points[ark.StatMap.Health].value == 9
-    assert profile2.header_size != 0
-    assert profile2.header_size == profile.header_size
+    def test_write_read(self):
+        profile = ark.ArkProfile()
+        profile.map_name = ark.GameMapMap.the_center
+        profile.map_path = ark.GameMapMap.the_center_path
+        profile.unique_id.set('11111111111111111')
+        profile.player_name.set('Fake Steam Name')
+        profile.player_id.set(utils._gen_player_id())
+        profile.first_spawned.set(True)
+        profile.character.name.set('GeneratedCharacter')
+        profile.character.level_ups.set(9)
+        profile.character.experience.set(450)
+        profile.character.stat_points[ark.StatMap.Health].set(9)
+        profile.save_to_file(output_dir + 'generatedprofile.arkprofile')
+
+        # TODO: Create some functionality to compare the data dicts directly
+        # to see if they are the same
+        profile2 = ark.ArkProfile(output_dir + 'generatedprofile.arkprofile')
+        assert profile2.map_name == profile.map_name
+        assert profile2.map_path == profile.map_path
+        assert profile2.unique_id.value == profile.unique_id.value
+        assert profile2.player_name.value == profile.player_name.value
+        assert profile2.player_id.value == profile.player_id.value
+        profile2_female = profile2.character.isFemale.value
+        profile_female = profile.character.isFemale.value
+        assert profile2_female == profile_female
+        assert profile2.character.stat_points[ark.StatMap.Health].value == 9
+        assert profile2.header_size != 0
+        assert profile2.header_size == profile.header_size
 
 
 class TestArkTribe:
-  def test_write_read(self):
-    owner_id = utils._gen_player_id()
-    owner_name = arktypes.StrProperty(value='Generated Owner')
-    tribe = ark.ArkTribe()
-    tribe.name.set('Generated Tribe')
-    tribe.tribe_id.set(utils._gen_tribe_id())
-    tribe.owner_id.set(owner_id)
-    tribe.members_names.value.append(owner_name)
-    member_id = arktypes.UInt32Property(value=owner_id)
-    tribe.members_ids.value.append(member_id)
-    tribe.save_to_file(output_dir + 'generatedtribe.arktribe')
 
-    tribe2 = ark.ArkTribe(output_dir + 'generatedtribe.arktribe')
-    assert tribe2.name.value == tribe.name.value
-    assert tribe2.tribe_id.value == tribe.tribe_id.value
-    assert tribe2.members_names.value[0].value == tribe.members_names.value[0].value
-    assert tribe2.owner_id.value == tribe.owner_id.value
+    def test_write_read(self):
+        owner_id = utils._gen_player_id()
+        owner_name = arktypes.StrProperty(value='Generated Owner')
+        tribe = ark.ArkTribe()
+        tribe.name.set('Generated Tribe')
+        tribe.tribe_id.set(utils._gen_tribe_id())
+        tribe.owner_id.set(owner_id)
+        tribe.members_names.value.append(owner_name)
+        member_id = arktypes.UInt32Property(value=owner_id)
+        tribe.members_ids.value.append(member_id)
+        tribe.save_to_file(output_dir + 'generatedtribe.arktribe')
+
+        tribe2 = ark.ArkTribe(output_dir + 'generatedtribe.arktribe')
+        assert tribe2.name.value == tribe.name.value
+        assert tribe2.tribe_id.value == tribe.tribe_id.value
+        assert tribe2.members_names.value[
+            0].value == tribe.members_names.value[0].value
+        assert tribe2.owner_id.value == tribe.owner_id.value
 
 
 @pytest.mark.slow
 class TestBulkReads:
-  def test_read_profiles(self):
-    path = 'data/Servers/Server01/'
-    files = os.listdir(path)
-    for file in files:
-      if '.arkprofile' in file:
-        try:
-          file_path = path + file
-          profile = ark.ArkProfile(file_path)
-        except:
-          print 'FAILED: %s' % file
-          assert False
-  def test_read_tribes(self):
-    path = 'data/Servers/Server01/'
-    files = os.listdir(path)
-    for file in files:
-      if '.arktribe' in file:
-        try:
-          file_path = path + file
-          tribe = ark.ArkTribe(file_path)
-        except:
-          print 'FAILED: %s' % file
-          assert False
-    assert True
+
+    def test_read_profiles(self):
+        path = 'data/Servers/Server01/'
+        files = os.listdir(path)
+        for file in files:
+            if '.arkprofile' in file:
+                try:
+                    file_path = path + file
+                    profile = ark.ArkProfile(file_path)
+                except:
+                    print 'FAILED: %s' % file
+                    assert False
+
+    def test_read_tribes(self):
+        path = 'data/Servers/Server01/'
+        files = os.listdir(path)
+        for file in files:
+            if '.arktribe' in file:
+                try:
+                    file_path = path + file
+                    tribe = ark.ArkTribe(file_path)
+                except:
+                    print 'FAILED: %s' % file
+                    assert False
+        assert True
 
 
 @pytest.mark.style
 class TestStyle:
+
     def test_pep8(self):
         pep8style = pep8.StyleGuide()
+        # result = pep8style.check_files([
+        #     'arkpy/ark.py',
+        #     'arkpy/arktypes.py',
+        #     'arkpy/tests/test_arktypes.py',
+        #     'arkpy/tests/test_ark.py',
+        #     'arkpy/binary.py',
+        #     'arkpy/utils.py'])
         result = pep8style.check_files([
-            'arkpy/ark.py',
-            'arkpy/arktypes.py',
-            'arkpy/binary.py',
-            'arkpy/utils.py'])
+          'tests/test_ark.py',
+          'tests/test_arktypes.py',
+          ])
         assert result.total_errors == 0
--- tests/test_arktypes.py
+++ tests/test_arktypes.py
@@ -8,229 +8,241 @@ output_dir = 'tests/output/'
 
 
 def write_and_read(file_name, prop):
-  file = output_dir + file_name
-  with open(file, 'wb') as ofile:
-    ostream = binary.BinaryStream(ofile)
-    prop._write_to_stream(ostream)
-  with open(file, 'rb') as ifile:
-    istream = binary.BinaryStream(ifile)
-    name, prop_type = istream.read_pair()
-    prop2 = prop.__class__(stream=istream)
-    prop2.var_name = name
-    return prop2
-
+    file = output_dir + file_name
+    with open(file, 'wb') as ofile:
+        ostream = binary.BinaryStream(ofile)
+        prop._write_to_stream(ostream)
+    with open(file, 'rb') as ifile:
+        istream = binary.BinaryStream(ifile)
+        name, prop_type = istream.read_pair()
+        prop2 = prop.__class__(stream=istream)
+        prop2.var_name = name
+        return prop2
 
 
 class TestStrProperty:
-  def test_init(self):
-    prop = arktypes.StrProperty('Something')
-    assert prop.value == 'Something'
 
-  def test_size(self):
-    new_value = 'One'
-    prop = arktypes.StrProperty(new_value)
-    size = prop._calc_inner_size()
-    assert size == len(new_value) + 5
+    def test_init(self):
+        prop = arktypes.StrProperty('Something')
+        assert prop.value == 'Something'
 
-  def test_total_size(self):
-    val = 'Woobles'
-    prop = arktypes.StrProperty(val)
-    prop.var_name = 'CharacterName'
-    size = prop._calc_size()
-    # Size of the Value, 4 bytes for length and 1 byte for null
-    thesize = len(val) + 5
-    # Size of the Variable name NTString
-    thesize = thesize + len(prop.var_name) + 5
-    # Size of the type NTString
-    thesize = thesize + len(prop.__class__.__name__) + 5
-    # Size of the two ints, size and index
-    thesize = thesize + 8
-    assert size == thesize
+    def test_size(self):
+        new_value = 'One'
+        prop = arktypes.StrProperty(new_value)
+        size = prop._calc_inner_size()
+        assert size == len(new_value) + 5
 
-  def test_write_read(self):
-    val = 'Woobles'
-    prop = arktypes.StrProperty(val)
-    prop.var_name = 'CharacterName'
-    prop._calc_inner_size()
-    prop2 = write_and_read('strproperty.output', prop)
-    assert prop2.value == prop.value
-    assert prop2.var_name == prop.var_name
+    def test_total_size(self):
+        val = 'Woobles'
+        prop = arktypes.StrProperty(val)
+        prop.var_name = 'CharacterName'
+        size = prop._calc_size()
+        # Size of the Value, 4 bytes for length and 1 byte for null
+        thesize = len(val) + 5
+        # Size of the Variable name NTString
+        thesize = thesize + len(prop.var_name) + 5
+        # Size of the type NTString
+        thesize = thesize + len(prop.__class__.__name__) + 5
+        # Size of the two ints, size and index
+        thesize = thesize + 8
+        assert size == thesize
+
+    def test_write_read(self):
+        val = 'Woobles'
+        prop = arktypes.StrProperty(val)
+        prop.var_name = 'CharacterName'
+        prop._calc_inner_size()
+        prop2 = write_and_read('strproperty.output', prop)
+        assert prop2.value == prop.value
+        assert prop2.var_name == prop.var_name
 
 
 class TestArrayProperty:
-  def test_init(self):
-    prop = arktypes.ArrayProperty()
-    assert len(prop.value) == 0
 
-  def test_write_read(self):
-    prop = arktypes.ArrayProperty()
-    prop2 = arktypes.IntProperty(value=2)
-    prop2.var_name = 'Test'
-    prop.var_name = 'TestArray'
-    prop.value.append(prop2)
-    prop._calc_size()
-    prop3 = write_and_read('arrayproperty.output', prop)
-    assert prop3.length == len(prop.value)
-    assert prop3.value[0].value == prop2.value
+    def test_init(self):
+        prop = arktypes.ArrayProperty()
+        assert len(prop.value) == 0
+
+    def test_write_read(self):
+        prop = arktypes.ArrayProperty()
+        prop2 = arktypes.IntProperty(value=2)
+        prop2.var_name = 'Test'
+        prop.var_name = 'TestArray'
+        prop.value.append(prop2)
+        prop._calc_size()
+        prop3 = write_and_read('arrayproperty.output', prop)
+        assert prop3.length == len(prop.value)
+        assert prop3.value[0].value == prop2.value
 
 
 class TestByteProperty:
-  def test_init(self):
-    prop = arktypes.ByteProperty(value=10)
-    assert prop.value == 10
 
-  def test_write_read(self):
-    prop = arktypes.ByteProperty(value=3)
-    prop.var_name = 'NumberOfLevelUpPointsApplied'
-    prop2 = write_and_read('byteproperty.output', prop)
-    assert prop2.value == prop.value
-    assert prop2.var_name == prop.var_name
+    def test_init(self):
+        prop = arktypes.ByteProperty(value=10)
+        assert prop.value == 10
+
+    def test_write_read(self):
+        prop = arktypes.ByteProperty(value=3)
+        prop.var_name = 'NumberOfLevelUpPointsApplied'
+        prop2 = write_and_read('byteproperty.output', prop)
+        assert prop2.value == prop.value
+        assert prop2.var_name == prop.var_name
 
 
 class TestObjectProperty:
-  def test_init(self):
-    prop = arktypes.ObjectProperty(value='campfire')
-    assert prop.value == 'campfire'
 
-  def test_write_read(self):
-    prop = arktypes.ObjectProperty(value='campfire')
-    prop.var_name = 'DefaultItemSlot'
-    prop._calc_size()
-    prop2 = write_and_read('objectproperty.output', prop)
-    assert prop2.value == prop.value
-    assert prop2.var_name == prop2.var_name
+    def test_init(self):
+        prop = arktypes.ObjectProperty(value='campfire')
+        assert prop.value == 'campfire'
+
+    def test_write_read(self):
+        prop = arktypes.ObjectProperty(value='campfire')
+        prop.var_name = 'DefaultItemSlot'
+        prop._calc_size()
+        prop2 = write_and_read('objectproperty.output', prop)
+        assert prop2.value == prop.value
+        assert prop2.var_name == prop2.var_name
 
 
 class TestFloatProperty:
-  def test_init(self):
-    prop = arktypes.FloatProperty(value=3)
-    assert prop.value == 3.0
-    assert isinstance(prop.value, float) == True
 
-  def test_write_read(self):
-    prop = arktypes.FloatProperty(value=0.75)
-    prop.var_name = 'AFloatValue'
-    prop._calc_size()
-    prop2 = write_and_read('floatproperty.output', prop)
-    assert prop2.value == prop.value
-    assert prop2.var_name == prop.var_name
+    def test_init(self):
+        prop = arktypes.FloatProperty(value=3)
+        assert prop.value == 3.0
+        assert isinstance(prop.value, float)
+
+    def test_write_read(self):
+        prop = arktypes.FloatProperty(value=0.75)
+        prop.var_name = 'AFloatValue'
+        prop._calc_size()
+        prop2 = write_and_read('floatproperty.output', prop)
+        assert prop2.value == prop.value
+        assert prop2.var_name == prop.var_name
 
 
 class TestDoubleProperty:
-  def test_init(self):
-    prop = arktypes.DoubleProperty(value=3)
-    assert prop.value == 3.0
-    assert isinstance(prop.value, float) == True
 
-  def test_write_read(self):
-    prop = arktypes.DoubleProperty(value=0.75)
-    prop.var_name = 'ADoubleValue'
-    prop._calc_size()
-    prop2 = write_and_read('doubleproperty.output', prop)
-    assert prop2.value == prop.value
-    assert prop2.var_name == prop.var_name
+    def test_init(self):
+        prop = arktypes.DoubleProperty(value=3)
+        assert prop.value == 3.0
+        assert isinstance(prop.value, float)
+
+    def test_write_read(self):
+        prop = arktypes.DoubleProperty(value=0.75)
+        prop.var_name = 'ADoubleValue'
+        prop._calc_size()
+        prop2 = write_and_read('doubleproperty.output', prop)
+        assert prop2.value == prop.value
+        assert prop2.var_name == prop.var_name
 
 
 class TestInt16Property:
-  def test_init(self):
-    prop = arktypes.Int16Property(value=3.0)
-    assert prop.value == 3
-    assert isinstance(prop.value, int) == True
 
-  def test_write_read(self):
-    prop = arktypes.Int16Property(value=5)
-    prop.var_name = 'AnInt16Value'
-    prop._calc_size()
-    prop2 = write_and_read('int16property.output', prop)
-    assert prop2.value == prop.value
-    assert prop2.var_name == prop.var_name
+    def test_init(self):
+        prop = arktypes.Int16Property(value=3.0)
+        assert prop.value == 3
+        assert isinstance(prop.value, int)
+
+    def test_write_read(self):
+        prop = arktypes.Int16Property(value=5)
+        prop.var_name = 'AnInt16Value'
+        prop._calc_size()
+        prop2 = write_and_read('int16property.output', prop)
+        assert prop2.value == prop.value
+        assert prop2.var_name == prop.var_name
 
 
 class TestUInt16Property:
-  def test_init(self):
-    prop = arktypes.UInt16Property(value=3.0)
-    assert prop.value == 3
-    assert isinstance(prop.value, int) == True
 
-  def test_write_read(self):
-    prop = arktypes.UInt16Property(value=5)
-    prop.var_name = 'AnUInt16Value'
-    prop._calc_size()
-    prop2 = write_and_read('uint16property.output', prop)
-    assert prop2.value == prop.value
-    assert prop2.var_name == prop.var_name
+    def test_init(self):
+        prop = arktypes.UInt16Property(value=3.0)
+        assert prop.value == 3
+        assert isinstance(prop.value, int)
+
+    def test_write_read(self):
+        prop = arktypes.UInt16Property(value=5)
+        prop.var_name = 'AnUInt16Value'
+        prop._calc_size()
+        prop2 = write_and_read('uint16property.output', prop)
+        assert prop2.value == prop.value
+        assert prop2.var_name == prop.var_name
 
 
 class TestIntProperty:
-  def test_init(self):
-    prop = arktypes.IntProperty(value=3.0)
-    assert prop.value == 3
-    assert isinstance(prop.value, int) == True
 
-  def test_write_read(self):
-    prop = arktypes.IntProperty(value=5)
-    prop.var_name = 'AnIntValue'
-    prop._calc_size()
-    prop2 = write_and_read('intproperty.output', prop)
-    assert prop2.value == prop.value
-    assert prop2.var_name == prop.var_name
+    def test_init(self):
+        prop = arktypes.IntProperty(value=3.0)
+        assert prop.value == 3
+        assert isinstance(prop.value, int)
+
+    def test_write_read(self):
+        prop = arktypes.IntProperty(value=5)
+        prop.var_name = 'AnIntValue'
+        prop._calc_size()
+        prop2 = write_and_read('intproperty.output', prop)
+        assert prop2.value == prop.value
+        assert prop2.var_name == prop.var_name
 
 
 class TestUInt32Property:
-  def test_init(self):
-    prop = arktypes.UInt32Property(value=3.0)
-    assert prop.value == 3
-    assert isinstance(prop.value, int) == True
 
-  def test_write_read(self):
-    prop = arktypes.UInt32Property(value=5)
-    prop.var_name = 'AUInt32Value'
-    prop._calc_size()
-    prop2 = write_and_read('uint32property.output', prop)
-    assert prop2.value == prop.value
-    assert prop2.var_name == prop.var_name
+    def test_init(self):
+        prop = arktypes.UInt32Property(value=3.0)
+        assert prop.value == 3
+        assert isinstance(prop.value, int)
+
+    def test_write_read(self):
+        prop = arktypes.UInt32Property(value=5)
+        prop.var_name = 'AUInt32Value'
+        prop._calc_size()
+        prop2 = write_and_read('uint32property.output', prop)
+        assert prop2.value == prop.value
+        assert prop2.var_name == prop.var_name
 
 
 class TestInt64Property:
-  def test_init(self):
-    prop = arktypes.Int64Property(value=3.0)
-    assert prop.value == 3
-    assert isinstance(prop.value, int) == True
 
-  def test_write_read(self):
-    prop = arktypes.Int64Property(value=5)
-    prop.var_name = 'AnInt64Value'
-    prop._calc_size()
-    prop2 = write_and_read('int64property.output', prop)
-    assert prop2.value == prop.value
-    assert prop2.var_name == prop.var_name
+    def test_init(self):
+        prop = arktypes.Int64Property(value=3.0)
+        assert prop.value == 3
+        assert isinstance(prop.value, int)
+
+    def test_write_read(self):
+        prop = arktypes.Int64Property(value=5)
+        prop.var_name = 'AnInt64Value'
+        prop._calc_size()
+        prop2 = write_and_read('int64property.output', prop)
+        assert prop2.value == prop.value
+        assert prop2.var_name == prop.var_name
 
 
 class TestUInt64Property:
-  def test_init(self):
-    prop = arktypes.UInt64Property(value=3.0)
-    assert prop.value == 3
-    assert isinstance(prop.value, int) == True
 
-  def test_write_read(self):
-    prop = arktypes.UInt64Property(value=5)
-    prop.var_name = 'AUInt64Value'
-    prop._calc_size()
-    prop2 = write_and_read('uint64property.output', prop)
-    assert prop2.value == prop.value
-    assert prop2.var_name == prop.var_name
+    def test_init(self):
+        prop = arktypes.UInt64Property(value=3.0)
+        assert prop.value == 3
+        assert isinstance(prop.value, int)
+
+    def test_write_read(self):
+        prop = arktypes.UInt64Property(value=5)
+        prop.var_name = 'AUInt64Value'
+        prop._calc_size()
+        prop2 = write_and_read('uint64property.output', prop)
+        assert prop2.value == prop.value
+        assert prop2.var_name == prop.var_name
 
 
 class TestBoolProperty:
-  def test_init(self):
-    prop = arktypes.BoolProperty(value=False)
-    assert prop.value == False
-    assert isinstance(prop.value, bool) == True
 
-  def test_write_read(self):
-    prop = arktypes.BoolProperty(value=True)
-    prop.var_name = 'SomeBooleanValue'
-    prop._calc_size()
-    prop2 = write_and_read('boolproperty.output', prop)
-    assert prop2.value == prop.value
-    assert prop2.var_name == prop.var_name
+    def test_init(self):
+        prop = arktypes.BoolProperty(value=False)
+        assert prop.value is False
+        assert isinstance(prop.value, bool)
+
+    def test_write_read(self):
+        prop = arktypes.BoolProperty(value=True)
+        prop.var_name = 'SomeBooleanValue'
+        prop._calc_size()
+        prop2 = write_and_read('boolproperty.output', prop)
+        assert prop2.value == prop.value
+        assert prop2.var_name == prop.var_name
